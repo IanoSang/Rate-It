dbf87ed100543cb0376df526ebc84a863ea7f597
IanoSang <ianosang18@gmail.com>
2022-06-12 14:14:12 +0300

    feat: views- adds a edit profile funtion to enable users change their user profile details

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import SignupForm
+from .forms import SignupForm, UpdateUserForm, UpdateUserProfileForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
@@ -28,7 +28,7 @@ def signup(request):
 
 @login_required(login_url='login')
 def profile(request, username):
-    return render(request, 'profile.html')
+    return render(request, 'rate/profile.html')
 
 
 def user_profile(request, username):
@@ -39,3 +39,23 @@ def user_profile(request, username):
         'user_prof': user_prof,
     }
     return render(request, 'rate/userprofile.html', context)
+
+
+@login_required(login_url='login')
+def edit_profile(request, username):
+    user = User.objects.get(username=username)
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+            return redirect('profile', user.username)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        prof_form = UpdateUserProfileForm(instance=request.user.profile)
+    context = {
+        'user_form': user_form,
+        'prof_form': prof_form
+    }
+    return render(request, 'rate/edit.html', context)
