3c8f2aeac253bfa666594ed60010badfae4aed42
IanoSang <ianosang18@gmail.com>
2022-06-12 14:26:48 +0300

    feat: models- adds a post model to store users post in app db

--- app/models.py
+++ app/models.py
@@ -27,3 +27,30 @@ class Profile(models.Model):
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=155)
+    url = models.URLField(max_length=255)
+    description = models.TextField(max_length=255)
+    technologies = models.CharField(max_length=200, blank=True)
+    photo = CloudinaryField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def search_project(cls, title):
+        return cls.objects.filter(title__icontains=title).all()
+
+    @classmethod
+    def all_posts(cls):
+        return cls.objects.all()
+
+    def save_post(self):
+        self.save()
