0d1e913476b76d25d0e9220d3b7688094b1f850d
IanoSang <ianosang18@gmail.com>
2022-06-12 14:12:53 +0300

    feat: urls- adds urls routes to the user profiles and user settings pages

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('account/', include('django.contrib.auth.urls')),
     path('profile/<username>/', views.profile, name='profile'),
+    path('<username>/profile', views.user_profile, name='userprofile'),
+    path('profile/<username>/settings', views.edit_profile, name='edit'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
