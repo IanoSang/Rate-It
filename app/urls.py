f5567d76cbeb22d4d5f9ff01e9852e75ac45440f
IanoSang <ianosang18@gmail.com>
2022-06-13 12:51:32 +0300

    feat: urls- adds a url path tpo the search results page

--- app/urls.py
+++ app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('profile/<username>/', views.profile, name='profile'),
     path('<username>/profile', views.user_profile, name='userprofile'),
     path('profile/<username>/settings', views.edit_profile, name='edit'),
+    path('search/', views.search_project, name='search'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
