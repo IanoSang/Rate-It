644f9dd40e1ae78cd478c68e8271cd9dc3958a9b
IanoSang <ianosang18@gmail.com>
2022-06-15 11:32:53 +0300

    feat: urls- adds a urls path to the profelist and project list json view page

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
@@ -11,7 +14,10 @@ urlpatterns = [
     path('profile/<username>/settings', views.edit_profile, name='edit'),
     path('search/', views.search_project, name='search'),
     path('project/<post>', views.project, name='project'),
+    path('profilelist', views.profile_list, name='profilelist'),
+    path('postlist', views.post_list, name='postlist'),
 
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
