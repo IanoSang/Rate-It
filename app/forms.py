dd3138a99b98906190817ac8bd935eb2b259e9cd
IanoSang <ianosang18@gmail.com>
2022-06-15 10:47:05 +0300

    feat: forms- adds a rating forms

--- app/forms.py
+++ app/forms.py
@@ -31,3 +31,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('photo', 'title', 'url', 'description', 'technologies',)
+
+
+class RatingsForm(forms.ModelForm):
+    class Meta:
+        model = Rating
+        fields = ['design', 'usability', 'content']
