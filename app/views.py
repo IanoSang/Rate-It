21c327eaece4c8268dd585e64f75c5d1918bb4a4
IanoSang <ianosang18@gmail.com>
2022-06-15 10:48:34 +0300

    feat: views- adds a view funtion to the the project page with the rating forms and rating calculation method

--- app/views.py
+++ app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import SignupForm, UpdateUserForm, UpdateUserProfileForm, PostForm
+from .forms import SignupForm, UpdateUserForm, UpdateUserProfileForm, PostForm, RatingsForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from .models import *
 from django.contrib.auth.models import User
 import random
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
@@ -99,3 +100,49 @@ def search_project(request):
     else:
         message = "You haven't searched for any Projects"
     return render(request, 'results.html', {'message': message})
+
+
+@login_required(login_url='login')
+def project(request, post):
+    post = Post.objects.get(title=post)
+    ratings = Rating.objects.filter(user=request.user, post=post).first()
+    rating_status = None
+    if ratings is None:
+        rating_status = False
+    else:
+        rating_status = True
+    if request.method == 'POST':
+        form = RatingsForm(request.POST)
+        if form.is_valid():
+            rate = form.save(commit=False)
+            rate.user = request.user
+            rate.post = post
+            rate.save()
+            post_ratings = Rating.objects.filter(post=post)
+
+            design_ratings = [d.design for d in post_ratings]
+            design_average = sum(design_ratings) / len(design_ratings)
+
+            usability_ratings = [us.usability for us in post_ratings]
+            usability_average = sum(usability_ratings) / len(usability_ratings)
+
+            content_ratings = [content.content for content in post_ratings]
+            content_average = sum(content_ratings) / len(content_ratings)
+
+            score = (design_average + usability_average + content_average) / 3
+            print(score)
+            rate.design_average = round(design_average, 2)
+            rate.usability_average = round(usability_average, 2)
+            rate.content_average = round(content_average, 2)
+            rate.score = round(score, 2)
+            rate.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = RatingsForm()
+    context = {
+        'post': post,
+        'rating_form': form,
+        'rating_status': rating_status
+
+    }
+    return render(request, 'rate/project.html', context)
